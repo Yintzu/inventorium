14768d94bb3393b1a237048130dc09ff0c6ecc2b
Filip Spånberg <filspa@labs2.org>
2022-06-15 16:17:15 +0200

    Added post function to locations

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -1,11 +1,25 @@
 import prisma from "../../services/prisma/client.ts"
 
 export default async function getAllLocations(req, res) {
-  try {
-    const locations = await prisma.locations.findMany()
-    res.status(200).json(locations)
-  } catch (error) {
-    console.error("Error talking to DB: ", error.message)
-    res.status(500).json({ error: error.message })
+  if (req.method === POST) {
+    const jsname = JSON.parse(req)
+    if (jsname != null && jsname.name != null && jsname.name != "") {
+      try {
+        const reply = await prisma.locations.create({
+          data: { name: jsname.name },
+        })
+      } catch (error) {
+        console.error("Error talking to DB: ", error.message)
+        res.status(500).json({ error: error.message })
+      }
+    }
+  } else {
+    try {
+      const locations = await prisma.locations.findMany()
+      res.status(200).json(locations)
+    } catch (error) {
+      console.error("Error talking to DB: ", error.message)
+      res.status(500).json({ error: error.message })
+    }
   }
 }
